e6741a7f2ce14c1ac38df9646623c7ad2b69ce76
Poutou <dutertre.adrien@pm.me>
2026-03-06 17:50:30 +0100

    end of migration to angular version 21

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,10 @@
-import { NgFor } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterModule, NgFor, RouterLinkActive],
+  imports: [RouterModule, RouterLinkActive],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
